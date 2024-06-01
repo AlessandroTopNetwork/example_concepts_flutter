aef65e6685fdb313c56d560154fda9c278fabb4b
Alessandro <alessandro.topnetwork@gmail.com>
2024-06-01 15:22:42 +0200

    image and asset

---
+++ root_dir/image_and_assets/android/app/src/main/kotlin/com/example/image_and_assets/image_and_assets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.image_and_assets.image_and_assets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ root_dir/image_and_assets/lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.red, // to see dimension of container
+              height: 300,
+              width: 300,
+              child: Image.asset(
+                "assets/images/field-533541_1280.jpg",
+                //height: 200,
+                //width: 200,
+                color: Colors.lightGreenAccent, // add color on image
+                colorBlendMode: BlendMode.exclusion, // fray image base with color
+                fit: BoxFit.none, // how fit image into container main
+                alignment: Alignment.bottomCenter, // alignament of image into container and it origin point see image
+                // alignment: Alignment.centerLeft,
+                // alignment: Alignment.topCenter,
+              ),
+              //Image(image: AssetImage("assets/images/field-533541_1280.jpg")),
+            ),
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            //Image.asset("assets/images/beach-1852945_1280.jpg"),
+            Image.network(
+                "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?q=80&w=1473&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                fit: BoxFit.fitHeight,
+            ), // get image from url link online
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
